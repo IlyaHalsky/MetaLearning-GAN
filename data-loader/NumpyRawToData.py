0c6b22785a256ec61459eeb7f2887dca58ab4192
Ilya <ilya.kachalsky@gmail.com>
2019-04-10 22:01:36 +0300

    NumpyToProcessed 3

--- data-loader/NumpyRawToData.py
+++ data-loader/NumpyRawToData.py
@@ -1,13 +1,14 @@
 import numpy as np
 from os import walk
 from tqdm import tqdm
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
 from pathlib import Path
 import pandas as pd
 import category_encoders as ce
+from numpy import unravel_index
 
 
 def load(name: str) -> (np.ndarray, np.ndarray):
@@ -34,6 +35,7 @@ def makeFeatures(labels_size: int, features_in: np.ndarray) -> np.ndarray:
     if features_in_size * (features_in_size - 1) / 2 < target_size - labels_size:
         degree = 10
         interaction_only = False
+        raise Exception(":(")
     else:
         degree = 2
         interaction_only = True
@@ -61,18 +63,60 @@ def shrinkFeatures(labels_size: int, features_in: np.ndarray, classes_in: np.nda
         return new_data
 
 
+def swapCol(data_in: np.array, labels_in: np.ndarray, frm: int, to: int) -> (np.ndarray, np.ndarray):
+    data_in[:, [frm, to]] = data_in[:, [to, frm]]
+    return data_in, labels_in
+
+
+def swapRow(data_in: np.array, labels_in: np.ndarray, frm: int, to: int) -> (np.ndarray, np.ndarray):
+    data_in[[frm, to], :] = data_in[[to, frm], :]
+    labels_in[[frm, to], :] = labels_in[[to, frm], :]
+    return data_in, labels_in
+
+
+def findMax(data_in: np.ndarray) -> (int, int):
+    return np.unravel_index(data_in.argmax(), data_in.shape)
+
+
+def sortData(data_in: np.ndarray, labels_in: np.ndarray) -> (np.ndarray, np.ndarray):
+    (rows_num, cols_num) = data_in.shape
+    data_out = np.copy(data_in)
+    labels_out = np.copy(labels_in)
+    for i in range(cols_num):
+        j = i
+        x_max, y_max = findMax(data_out[i:, j:])
+        x_max += i
+        y_max += j
+        (data_out, labels_out) = swapRow(data_out, labels_out, i, x_max)
+        (data_out, labels_out) = swapCol(data_out, labels_out, j, y_max)
+
+    return data_out, labels_out
+
+
+def prepareData(data_in: np.ndarray, labels_in: np.ndarray) -> (np.ndarray, np.ndarray):
+    data_in = data_in.astype(np.float64)
+    labels_in = labels_in.astype(np.float64)
+    min_max_scaler = MinMaxScaler()
+    data_out = min_max_scaler.fit_transform(data_in)
+    min_max_scaler = MinMaxScaler()
+    labels_out = min_max_scaler.fit_transform(labels_in)
+    return data_out, labels_out
+
+
 def writeData(name_in: str, data_in: np.ndarray, labels_in: np.ndarray) -> bool:
+    data_prepped, labels_prepped = prepareData(data_in, labels_in)
+    data_sorted, labels_sorted = sortData(data_prepped, labels_prepped)
     raw_path = f'{done_data_str_raw}/{name_in}/'
     raw_name = f'{raw_path}{name_in}'
     path = Path(raw_path)
     path.mkdir(parents=True, exist_ok=True)
-    (_, labels_width) = labels_in.shape
-    (_, data_width) = data_in.shape
+    (_, labels_width) = labels_sorted.shape
+    (_, data_width) = data_sorted.shape
     raw_data_name = f'{raw_name}_{data_width}_data'
     raw_labels_name = f'{raw_name}_{labels_width}_labels'
-    np.save(raw_data_name, data_in)
-    np.save(raw_labels_name, labels_in)
-    data_out = np.concatenate((labels_in, data_in), axis=1)
+    np.save(raw_data_name, data_sorted)
+    np.save(raw_labels_name, labels_sorted)
+    data_out = np.concatenate((labels_sorted, data_sorted), axis=1)
     data_name = f'{done_data_str}{name_in}_{labels_width}_{data_width}'
     np.save(data_name, data_out)
     return True
@@ -89,22 +133,32 @@ def prepare(dataset_name: str, dataset_in: (np.ndarray, np.ndarray), pbar: tqdm)
         choices = np.random.choice(instances_size, target_size, replace=True)
         data_in = data_in[choices]
         labels_in = labels_in[choices]
+        instances_size = target_size
 
+    labels_out = labels_in
     if features_size + labels_size < target_size:
-        data_out = makeFeatures(labels_size, data_in)
+        choices = np.random.choice(instances_size, target_size, replace=False)
+        data_out = data_in[choices]
+        labels_out = labels_in[choices]
+        instances_size = target_size
+        try:
+            data_out = makeFeatures(labels_size, data_out)
+        except:
+            return False
     elif features_size + labels_size == target_size:
         data_out = data_in
     else:
         data_out = shrinkFeatures(labels_size, data_in, labels_in)
 
-    data_to_write = data_out[:target_size]
-    labels_to_write = labels_in[:target_size]
+    choices = np.random.choice(instances_size, target_size, replace=False)
+    data_to_write = data_out[choices]
+    labels_to_write = labels_out[choices]
     result = writeData(dataset_name, data_to_write, labels_to_write)
     return result
 
 
 if __name__ == '__main__':
-    target_size = 50
+    target_size = 1000
     raw_data_str = "./datasets/np_raw/"
     done_data_str = f"./datasets/processed_{target_size}/"
     done_data_str_raw = f"./datasets/processed_{target_size}/raw"
--- processing/Test.py
+++ processing/Test.py
@@ -1,12 +1,6 @@
 import numpy as np
 
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-Y = np.array([[1, 0], [1, 0], [1, 0], [1, 1], [1, 1], [1, 1]])
-from sklearn.naive_bayes import GaussianNB
-
-clf = GaussianNB()
-clf.fit(X, Y)
-print(clf.predict([[-0.8, -1]]))
-clf_pf = GaussianNB()
-clf_pf.partial_fit(X, Y, np.unique(Y))
-print(clf_pf.predict([[-0.8, -1]]))
+if __name__ == '__main__':
+    a = np.array([[1, 2, 4], [4, 5, 6]])
+    print(a.argmax())
+    print(np.unravel_index(a.argmax(), a.shape))
